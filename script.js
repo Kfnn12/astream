3078ccc809054abad7c57dfbe87ec20760debe8c
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2023-11-21 12:21:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var dataTitle;
 var dataEpisode;
 var dataURL;
 
-const apiEndpoint = "https://api.consumet.org";
+const apiEndpoint = "https://animxer-api-nxqu.vercel.app";
 
 // Randomly set player iframes to these video onLoad
 var youtubeLinks = [
